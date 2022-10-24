9f5a4c812f645804afc2f3ad7c3f69127e1f14e0
dedev5 <dedev5430@gmail.com>
2022-10-23 23:55:27 -0400

    A6 bug fixes 3

--- src/labs/index.js
+++ src/labs/index.js
@@ -1,10 +1,30 @@
 import Assignment6 from "./a6";
 import Nav from "../nav";
+
+
+
+function Classes() {
+    const color = 'blue';
+    const dangerous = true;
+    return (
+        <div>
+            <h2>Classes</h2>
+            <div className={`${dangerous ? 'wd-bg-red' : 'wd-bg-green'}
+                                   wd-fg-black wd-padding-10px`}>
+                Dangerous background
+            </div>
+            <div className={`wd-bg-${color} wd-fg-black wd-padding-10px`}>
+                Dynamic Blue background</div>
+
+        </div>
+    );
+};
 function Labs() {
     return (
         <div>
             <Nav/>
             <Assignment6/>
+            <Classes/>
         </div>
     );
 }
