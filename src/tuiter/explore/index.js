f900b56deef55e20f07234b256e872757aaa088b
dedev5 <dedev5430@gmail.com>
2022-10-23 23:51:11 -0400

    A6 bug fixes 3

--- src/tuiter/explore/index.js
+++ src/tuiter/explore/index.js
@@ -33,7 +33,7 @@ const ExploreComponent = () => {
                 </li>
             </ul>
             <div className="position-relative mb-2">
-                <img alt="A large photo of Spacex's starship prototype." src="https://techcrunch.com/wp-content/uploads/2019/09/Starship-Mk1-Day.jpg" className="w-100"/>
+                <img alt="Spacex's starship prototype." src="https://techcrunch.com/wp-content/uploads/2019/09/Starship-Mk1-Day.jpg" className="w-100"/>
                 <h1 className="position-absolute wd-nudge-up text-white">
                     SpaceX Starship</h1>
             </div>
