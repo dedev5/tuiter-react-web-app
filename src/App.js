68df018459e600153025e90aa7693c740db44109
dedev5 <dedev5430@gmail.com>
2022-11-07 01:17:40 -0500

    A7 fixes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <BrowserRouter>
             <div className="container">
                 <Routes>
-                    <Route path="/"
+                    <Route path="/*"
                            element={<Labs/>}/>
                     <Route path="/hello"
                            element={<HelloWorld/>}/>
