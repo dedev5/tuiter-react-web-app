633ec3b0514e65f113a80be263c173ebf53016f4
dedev5 <dedev5430@gmail.com>
2022-11-20 22:11:01 -0500

    Removing unused imports

--- src/tuiter/tuits/tuits-reducer.js
+++ src/tuiter/tuits/tuits-reducer.js
@@ -1,5 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-import tuits from './tuits.json';
 import {updateTuitThunk, createTuitThunk, deleteTuitThunk, findTuitsThunk}
     from "../../services/tuits-thunks";
 
@@ -7,6 +6,7 @@ const initialState = {
     tuits: [],
     loading: false
 }
+
 const currentUser = {
     "userName": "NASA",
     "handle": "@nasa",
